5e7e7a6327b5bbedc0dd6fe1a8e4bbffc71cea4d
lauragonzalezcomalada <lauragonzalezcomalada@gmail.com>
2025-10-03 15:16:50 -0300

    changes pel mapa

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
 import UIKit
 import Flutter
+import GoogleMaps  
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyBM51UAqo5azY443B3CxM8VMv-IIRLIOR0")  
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/widgets/mapafrombckdng.dart
+++ lib/widgets/mapafrombckdng.dart
@@ -7,62 +7,37 @@ class MapaDesdeBackend extends StatefulWidget {
   final String? imageUrl;
 
   const MapaDesdeBackend(
-      {required this.lat,
-      required this.long,
-      this.imageUrl,
-      super.key});
+      {required this.lat, required this.long, this.imageUrl, super.key});
 
   @override
   State<MapaDesdeBackend> createState() => _MapaDesdeBackendState();
 }
 
 class _MapaDesdeBackendState extends State<MapaDesdeBackend> {
-  //final Set<Marker> _markers = {};
-  /*BitmapDescriptor customIcon = BitmapDescriptor.defaultMarker;
-
-  @override
-  void initState() {
-    super.initState();
-    customMarker();
-  }
-
-  void customMarker() async {
-    final icon = await BitmapDescriptor.asset(
-      const ImageConfiguration(size: Size(5, 5)),
-      "assets/boton_sol.png",
-    );
-
-    setState(() {
-      customIcon = icon;
-      _markers.add(
-       
-    });
-    print(_markers);
-  }*/
-
   @override
   Widget build(BuildContext context) {
     final LatLng ubicacion = LatLng(widget.lat, widget.long);
 
     return SizedBox(
-      height: 100, // altura fija
+      height: 100,
       width: double.infinity,
-
       child: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: ubicacion,
           zoom: 14,
         ),
-        markers: { Marker(
-          markerId: const MarkerId('lugar_marker'),
-          position: LatLng(widget.lat, widget.long),
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-          infoWindow: const InfoWindow(
-            title: 'Lugar',
-            snippet: 'Aquí está el marcador',
+        markers: {
+          Marker(
+            markerId: const MarkerId('lugar_marker'),
+            position: LatLng(widget.lat, widget.long),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueOrange),
+            infoWindow: const InfoWindow(
+              title: 'Lugar',
+              snippet: 'Aquí está el marcador',
+            ),
           ),
-        ),
-      },
+        },
       ),
     );
   }
